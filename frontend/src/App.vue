fd61349d008bf36854d813f7213029e37654301c
Angelus Rocha <angelusrt@gmail.com>
2024-03-20 11:07:10 -0300

    organizando front-end

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
@@ -9,24 +9,43 @@
 </template>
 
 <style>
+* {
+  background-color: #1f2d40;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
 }
 
 nav {
-  padding: 30px;
+  padding: 30px 30px;
+  background-color: #2c3e50;
+  border-radius: 20px;
+  margin: 10px auto 20px auto;
+  max-width: 200px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  font-weight: normal;
+  background-color: #2c3e50;
+  color: #fff;
+  padding: 10px 20px;
+  border-radius: 10px;
+  margin: 0 5px;
+  font-style: normal;
+  text-decoration: none;
+  border: solid 2px #1f2d40;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  font-weight: bold;
+  color: #2c3e50;
+  border-color: #42b983;
+  background-color: #42b983;
 }
 </style>
